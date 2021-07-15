75edb890c343e27faf5288f205ed1a0e728afd7f
Hashan <hashan2@hotmail.com>
2021-07-15 15:38:04 +0100

    Added Bitmat Drawing

--- SSD1306Core/SSD1306.cs
+++ SSD1306Core/SSD1306.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using WPICore;
 using WPISPI;
+using System.Drawing;
 namespace SSD1306Core
 {
     public class SSD1306
@@ -32,7 +33,7 @@ namespace SSD1306Core
 
 	    static SSD1306() {
 		    Core.WiringPiSetup();
-		    spi = new (0, 500000);
+		    spi = new (0, 2000000);
 		    Core.PinMode(8, PinMode.OUTPUT);
 		    Core.PinMode(9, PinMode.OUTPUT);
 		    Core.DigitalWrite(9, PinState.HIGH);
@@ -52,6 +53,45 @@ namespace SSD1306Core
 		   for(int i = 0; i < 1024; i++){
 			   SendData(new byte[]{clearingByte});
 		   }
+	    }
+
+	    public static void DrawBitmapFrame(Bitmap bmp){
+		    byte pixel = 0x00;
+		    for(int i = 0; i < bmp.Height; i+=8){
+			   for(int j = 0; j < bmp.Width; j++){
+				   var c0 = bmp.GetPixel(j, i+0);
+				   var c1 = bmp.GetPixel(j, i+1);
+				   var c2 = bmp.GetPixel(j, i+2);
+				   var c3 = bmp.GetPixel(j, i+3);
+				   var c4 = bmp.GetPixel(j, i+4);
+				   var c5 = bmp.GetPixel(j, i+5);
+				   var c6 = bmp.GetPixel(j, i+6);
+				   var c7 = bmp.GetPixel(j, i+7);
+				   
+				   var d0 = (c0.R + c0.G + c0.B) / 3;
+				   var d1 = (c1.R + c1.G + c1.B) / 3;
+				   var d2 = (c2.R + c2.G + c2.B) / 3;
+				   var d3 = (c3.R + c3.G + c3.B) / 3;
+				   var d4 = (c4.R + c4.G + c4.B) / 3;
+				   var d5 = (c5.R + c5.G + c5.B) / 3;
+				   var d6 = (c6.R + c6.G + c6.B) / 3;
+				   var d7 = (c7.R + c7.G + c7.B) / 3;
+
+				   pixel = d0 > 0?(byte) 1 : (byte)0;
+				   pixel += d1 > 0? (byte)2 : (byte)0;
+				   pixel += d2 > 0? (byte)4 : (byte)0;
+				   pixel += d3 > 0? (byte)8 : (byte)0;
+				   pixel += d4 > 0? (byte)16 : (byte)0;
+				   pixel += d5 > 0? (byte)32 : (byte)0;
+				   pixel += d6 > 0? (byte)64 : (byte)0;
+				   pixel += d7 > 0? (byte)128 : (byte)0;
+				  
+				   SendData(new byte[]{pixel});
+				   //Console.WriteLine(pixel.ToString("X2"));
+				   pixel = 0x00;
+			   }
+		   }
+
 	    }
 
 	    private static void SendCommand(byte[] command){
--- SSD1306Driver/Program.cs
+++ SSD1306Driver/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using SSD1306Core;
+using System.Drawing;
+using System.Threading;
 namespace SSD1306Driver
 {
     class Program
     {
         static void Main(string[] args)
         {
-		SSD1306.Dummy();
+		var bmp = new Bitmap("");
 		SSD1306.ClearDisplay();
+		SSD1306.DrawBitmapFrame(bmp);
+
         }
     }
 }
